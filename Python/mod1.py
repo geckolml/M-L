f38b2ab37f3855a83697b28185e9c7b1be4deaa5
C-Lara <claraa@uni.edu.pe>
2017-03-25 14:48:53 -0500

    Eliminando directorio

--- Python/mod1.py
+++
@@ -1,6 +0,0 @@
-# Archivo mod1.py
-
-"""
-Ejemplo de un módulo
-"""
-print("hola: yo soy un módulo")
--- Python/mod2.py
+++
@@ -1,8 +0,0 @@
-# Archivo mod2.py
-
-import mod1
-import sys
-
-print (__name__)
-print (mod1.__name__)
-print(sys.__name__)
